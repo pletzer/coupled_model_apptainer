3a3814823f14e965f9817d235704b4c4bd11d0db
Andrew Pauling <andrewpauling109@gmail.com>
2025-12-12 13:26:45 +1300

    Reapply "add 3 component ESMF example"
    This reverts commit 4722ae0516f67a7e3bef3690d64bae4f46eee27a.

---
+++ examples/3comps/build/CMakeFiles/2comp_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2comp_example"
+  "2comp_example.pdb"
+  "CMakeFiles/2comp_example.dir/2comp_example.f90.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/2comp_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/3comps/build/CMakeFiles/3comp_time_example.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for 3comp_time_example.
+# This may be replaced when dependencies are built.
